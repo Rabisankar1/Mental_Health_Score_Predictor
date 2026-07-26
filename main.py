79bf97cbc1408894ee5c78063e21e6e9fed8deb3
Rabisankar <pradhanrabisankar328@gmail.com>
2026-07-26 12:17:57 +0530

    first commit

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ class PredictionResponse(BaseModel):
 
 @app.get('/')
 def greet():
-    return {'Welcome to Code with Rabi Guys'}
+    return {'Welcome to Sheryians AI School Guys'}
 
 
 @app.post('/predict', response_model=PredictionResponse) #6.77777
